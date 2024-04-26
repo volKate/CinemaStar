e673f28951680a2dbe1274545ea37b7071acff5f
Ekaterina V <katee.volkova@gmail.com>
2024-04-26 18:30:08 +0200

    feat: implement saving isFavorite

--- CinemaStar/CinemaStar/App/Details/View/DetailsViewController.swift
+++ CinemaStar/CinemaStar/App/Details/View/DetailsViewController.swift
@@ -9,11 +9,27 @@ final class DetailsViewController: UIViewController {
     private let detailsViewModel: DetailsViewModelProtocol?
 
     private let detailsTableView = DetailsTableView()
+    private lazy var heartBarButtonItem: UIBarButtonItem = {
+        let item = UIBarButtonItem(
+            title: nil,
+            image: UIImage(systemName: "heart"),
+            target: self,
+            action: #selector(handleFavoriteTapped)
+        )
+        return item
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         detailsViewModel?.fetchMovieDetails()
+        setupBindings()
+    }
+
+    private func setupBindings() {
+        detailsViewModel?.isFavorite.bind { [weak self] isFavorite in
+            self?.heartBarButtonItem.image = UIImage(systemName: isFavorite ? "heart.fill" : "heart")
+        }
     }
 
     private func setupView() {
@@ -23,8 +39,17 @@ final class DetailsViewController: UIViewController {
             detailsTableView.configure(with: detailsViewModel)
         }
 
+        view.backgroundColor = .white
         view.layer.insertSublayer(AppBackgroundGradientLayer(frame: view.bounds), at: 0)
         detailsTableView.backgroundColor = .clear
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.rightBarButtonItem = heartBarButtonItem
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
 
     private func setupConstraints() {
@@ -41,5 +66,9 @@ final class DetailsViewController: UIViewController {
     required init?(coder: NSCoder) {
         detailsViewModel = nil
         super.init(coder: coder)
+    }
+
+    @objc private func handleFavoriteTapped() {
+        detailsViewModel?.handleToggleFavorite()
     }
 }
